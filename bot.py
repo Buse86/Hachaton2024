fb7ffa70130dbf0de2261854e7ec4eeaa82f0ace
k <ivan.dem371539@gmail.com>
2024-11-26 19:17:21 +0500

    авторизация с вайлом

--- bot.py
+++ bot.py
@@ -31,33 +31,34 @@ headers_login = {'Content-Type': 'application/json'}
 async def StartLogin(message: Message):
     await message.answer('Привет! Для продолжения работы нужно авторизоваться')
     await message.answer('Для авторизации напишите /login (логин) (пароль)')
+    while True:
+        if message.from_user.id not in users.index:
+            @dp.message(Command('login'))
+            async def asd(message: Message):
+                log = message.text[6:].split()
 
-    if message.from_user.id not in users.index:
-        @dp.message(Command('login'))
-        async def asd(message: Message):
-            log = message.text[6:].split()
+                json_login = {
+                    "login": log[0],
+                    "password": log[1],
+                    "fingerprint": {}
+                }
 
-            json_login = {
-                "login": log[0],
-                "password": log[1],
-                "fingerprint": {}
-            }
+                ans = requests.post(url_login, headers=headers_login, json=json_login).json()
 
-            ans = requests.post(url_login, headers=headers_login, json=json_login).json()
-
-            try:
-                await message.answer(f'Вы успешно авторизовались!')
-                print(users)
-                if str(message.from_user.id) not in users.index:
-                    users.loc[str(message.from_user.id)] = log
-                    users.to_csv('log.csv', index=False)
+                try:
+                    await message.answer(f'Вы успешно авторизовались!')
                     print(users)
+                    if str(message.from_user.id) not in users.index:
+                        users.loc[str(message.from_user.id)] = log
+                        users.to_csv('log.csv', index=False)
+                        print(users)
 
-            except:
-                await message.answer(ans['detail'])
-    else:
-        @dp.message(Command('VCS'))
-        async def VCS(message: Message)):
+                except:
+                    await message.answer(ans['detail'] + 'Попробуйте снова')
+
+        else:
+            @dp.message(Command('VCS'))
+            async def VCS(message: Message)):
 
 
 

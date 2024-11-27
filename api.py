044d979374229a44f47ec32c93746901c72c533d
k <ivan.dem371539@gmail.com>
2024-11-27 13:59:57 +0500

    готовая авторизация даты

--- api.py
+++ api.py
@@ -1,33 +1,108 @@
+import asyncio
+from operator import index
+from pyexpat.errors import messages
+
 import requests
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils import *
+import requests
+import logging
+import pandas as pd
 
-url_event = 'https://test.vcc.uriit.ru/api/meetings'
-url_login = 'https://test.vcc.uriit.ru/api/auth/login'
+# message.from_user.id
+
+#users = pd.read_csv('log.csv')
+users = pd.DataFrame(columns=['login', 'password'])
+
 url_vcs = 'https://test.vcc.uriit.ru/api/meetings'
+url_login = 'https://test.vcc.uriit.ru/api/auth/login'
 
-json_login = {
-  "login": "hantaton08",
-  "password": "apyNbPS5Wr8^PbCg",
-  "fingerprint": {}
-}
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token='7547176072:AAHuhJPMFgORV224QwJVAxjonJBuv3TqL7I')
+dp = Dispatcher()
 
 headers_login = {'Content-Type': 'application/json'}
 
-token = requests.post(url_login, headers=headers_login, json=json_login).json()['token']
+# token = requests.post(url_login, headers=headers_login, json=json_login).json()['token']
 
-headers = {
-    'Authorization': 'Bearer ' + token,
-    'Content-Type': 'application/json'
-}
+@dp.message(Command('start'))
+async def StartLogin(message: Message):
+    await message.answer('Привет! Для продолжения работы нужно авторизоваться')
+    await message.answer('Для авторизации напишите /login (логин) (пароль)')
+    @dp.message(Command('login'))
+    async def asd(message: Message):
+        if str(message.from_user.id) not in users.index:
+            log = message.text[6:].split()
 
-event = requests.get(url_event, headers=headers)
+            json_login = {
+                "login": log[0],
+                "password": log[1],
+                "fingerprint": {}
+            }
 
-params_vcs = {
-    'fromDatetime': '2024-11-14T00:23:10.028081',
-    'toDatetime': '2024-11-14T20:23:10.028122'
-}
+            ans = requests.post(url_login, headers=headers_login, json=json_login).json()
 
-info_vcs = ['name', 'startedAt', 'endedAt', 'duration', 'organizedBy', ]
+            try:
+                tmp = requests.post(url_login, headers=headers_login, json=json_login).json()['token']
+                try:
+                    print(users['898461114'])
+                    await message.answer(users.index)
+                except:
+                    pass
+                if str(message.from_user.id) not in users.index:
+                    users.loc[str(message.from_user.id)] = log
+                    users.to_csv('log.csv')
+                    print(users)
+                await message.answer(f'Вы успешно авторизовались!')
+            except:
+                await message.answer(ans['detail'] + '\nПопробуйте снова')
+        else:
+            await message.answer('Вы авторизованы')
 
-vcs = requests.get(url_vcs, headers=headers, params=params_vcs)
+@dp.message(Command('vcs'))
+async def VCSInfo(message: Message):
+    if str(message.from_user.id) in users.index:
+        #await message.answer('Введите даты начала и окончания ВКС через пробел, в формате: ГГГГ-ММ-ДД')
+        try:
+            data = str(message.text)[5:].split()
 
-print(vcs.json()['data'])
+            jsonlogin = {
+                "login": str(users.loc[str(message.from_user.id), "login"]),
+                "password": str(users.loc[str(message.from_user.id), "password"]),
+                "fingerprint": {}
+            }
+
+            #print(jsonlogin['login'], jsonlogin['password'])
+
+            token = requests.post(url_login, headers=headers_login, json=jsonlogin).json()['token']
+
+            print(token)
+
+            headers = {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            }
+
+            params_vcs = {
+                'fromDatetime': f'2024-11-14T00:23:10.028081',
+                'toDatetime': f'2024-11-30T20:23:10.028122'
+            }
+
+            info_vcs = ['name', 'startedAt', 'endedAt', 'duration', 'organizedBy']
+            vcs = requests.get(url_vcs, headers=headers, params=params_vcs)
+
+            print(vcs)
+
+            await message.answer(vcs.text)
+        except:
+            await message.answer('Вы ввели некорректную дату')
+
+async def main():
+    await dp.start_polling(bot)
+
+asyncio.run(main())

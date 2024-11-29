0730b7a339285c6956b9ae87481078eaec5d7f61
k <ivan.dem371539@gmail.com>
2024-11-30 03:07:52 +0500

    Готовый поиск ВКС с моим участием(Все охуенно)

--- bot.py
+++ bot.py
@@ -126,6 +126,78 @@ async def VCSInfo(message: Message):
         except:
             await message.answer('Вы ввели некорректную дату')
 
+@dp.message(Command('myvcs'))
+async def myVCSInfo(message: Message):
+    if np.int64(message.from_user.id) in users['id'].values:
+        # await message.answer('Введите даты начала и окончания ВКС через пробел, в формате: ГГГГ-ММ-ДД')
+        try:
+            data = str(message.text)[7:].split()
+            print(1)
+            jsonlogin = {
+                "login": str(users.loc[users['id'].values.tolist().index(message.from_user.id), 'login']),
+                "password": str(users.loc[users['id'].values.tolist().index(message.from_user.id), 'password']),
+                "fingerprint": {}
+            }
+
+
+            # print(jsonlogin['login'], jsonlogin['password'])
+
+            token = requests.post(url_login, headers=headers_login, json=jsonlogin).json()['token']
+
+            # print(token)
+
+
+            headers = {
+                'Authorization': 'Bearer ' + str(token),
+                'Content-Type': 'application/json',
+            }
+
+            vcs_ans = []
+            info_vcs = ['name', 'startedAt', 'endedAt', 'duration', 'organizedBy']
+            for i in ['started', 'ended', 'booked', 'cancelled']:
+                id1 = requests.post(url_login, headers=headers_login, json=jsonlogin).json()['user']['id']
+                print(2)
+                params_vcs = {
+                    'fromDatetime': f'{data[0]}T00:00:00.000000',
+                    'toDatetime': f'{data[1]}T23:59:00.000000',
+                    'state': i,
+                    'userParticipant': int(id1)
+                }
+                vcs = requests.get(url_vcs, headers=headers, params=params_vcs)
+                vcs_ans += vcs.json()['data']
+                # print(vcs.json()['data'])
+            nm = {
+                'name': 'Название:',
+                "startedAt": 'Начало: ',
+                'endedAt': 'Конец',
+                'duration': 'Продолжительность',
+                'organizedBy': 'Организованно'
+            }
+            # print(vcs_ans)
+            for i in vcs_ans:
+                print(3)
+                try:
+                    org = requests.get('https://test.vcc.uriit.ru/api/catalogs/departments/' + i['organizedBy'], headers=headers, params={'department_id': i['organizedBy']}).json()['name']
+                    await message.answer(f'Название: {i['name']}\nДата начала: {i['startedAt'][:10]} {i['startedAt'][11:-3]}\nДата окончания: {i['endedAt'][:10]} {i['endedAt'][11:-3]}\nПродолжительность: {i['duration']} минут\nОрганизованно: {org}')
+                except:
+                    await message.answer(f'Название: {i['name']}\nДата начала: {i['startedAt'][:10]} {i['startedAt'][11:-3]}\nДата окончания: {i['endedAt'][:10]} {i['endedAt'][11:-3]}\nПродолжительность: {i['duration']} минут')
+
+        except:
+            await message.answer('Вы ввели некорректную дату')
+
+
+@dp.message(Command('filter'))
+async def FilterVCS(message: Message):
+    await message.answer('Выберите, по каким значениям хотите сортировать ВКС:\n'
+                         'Забронированные - /zabron\n'
+                         'Начатые - /active\n'
+                         'Законченные - /zakonchen\n'
+                         'Отмененные - /otmen\n'
+                         'Наименование - /imya\n'
+                         'Приоритет - /prior\n'
+                         'Департамент - /departament\n'
+                         'Организатор - /organiz')
+
 @dp.message(Command('create'))
 async def CreateVcs(message: Message):
     users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 1
@@ -139,113 +211,117 @@ async def CanсelVcs(message: Message):
 
 @dp.message()
 async def CreateVcs1(message: Message):
-    if users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 1:
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 2
-        response = message.text
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] = response
-        users.to_csv('log.csv', index=False)
-        await message.answer('Введите время начала конференции (гггг-мм-дд чч:мм)')
+    if np.int64(message.from_user.id) in users['id'].values:
+        if users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 1:
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 2
+            response = message.text
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] = response
+            users.to_csv('log.csv', index=False)
+            await message.answer('Введите время начала конференции (гггг-мм-дд чч:мм)')
 
-    elif users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 2:
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 3
+        elif users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 2:
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 3
 
-        response = message.text
-        tmp = response.split()[1]
-        response = response.replace(' ', f'T{tmp}:00.000000')[:-5]
+            response = message.text
+            tmp = response.split()[1]
+            response = response.replace(' ', f'T{tmp}:00.000000')[:-5]
 
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] = users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] + ';' + str(response)
-        users.to_csv('log.csv', index=False)
-        await message.answer('Введите максимальное кол-во участников конференции')
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] = users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] + ';' + str(response)
+            users.to_csv('log.csv', index=False)
+            await message.answer('Введите максимальное кол-во участников конференции')
 
-    elif users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 3:
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 4
+        elif users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 3:
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 4
 
-        response = message.text
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] += ';' + response
-        users.to_csv('log.csv', index=False)
-        await message.answer('Введите продoлжительность конференции в минутах')
+            response = message.text
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] += ';' + response
+            users.to_csv('log.csv', index=False)
+            await message.answer('Введите продoлжительность конференции в минутах')
 
-    elif users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 4:
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 5
-        response = message.text
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] += ';' + response
-        users.to_csv('log.csv', index=False)
-        await message.answer('''Отправтье любое сообщение для подтверждения создания ВКС''')
+        elif users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 4:
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 5
+            response = message.text
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'] += ';' + response
+            users.to_csv('log.csv', index=False)
+            await message.answer('''Отправтье любое сообщение для подтверждения создания ВКС''')
 
-    elif users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 5:
-        users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 0
-        zxc = users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'].split(';')
+        elif users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] == 5:
+            users.loc[users['id'].values.tolist().index(message.from_user.id), 'сreate'] = 0
+            zxc = users.loc[users['id'].values.tolist().index(message.from_user.id), 'VCScreate'].split(';')
 
-        if len(zxc) >= 4:
-            try:
-                zxc[2] = int(zxc[2])
-                zxc[3] = int(zxc[3])
-            except ValueError:
-                await message.answer('Ошибка при преобразовании данных. Убедитесь, что время и продолжительность указаны верно.')
+            if len(zxc) >= 4:
+                try:
+                    zxc[2] = int(zxc[2])
+                    zxc[3] = int(zxc[3])
+                except ValueError:
+                    await message.answer('Ошибка при преобразовании данных. Убедитесь, что время и продолжительность указаны верно.')
+                    return
+            else:
+                await message.answer('Недостаточно данных для создания события.')
                 return
-        else:
-            await message.answer('Недостаточно данных для создания события.')
-            return
 
-        # print("New data array (zxc):", zxc)
+            # print("New data array (zxc):", zxc)
 
-        jsonlogin = {
-            "login": str(users.loc[users['id'].values.tolist().index(message.from_user.id), 'login']),
-            "password": str(users.loc[users['id'].values.tolist().index(message.from_user.id), 'password']),
-            "fingerprint": {}
-        }
-
-        try:
-            response = requests.post(url_login, headers=headers_login, json=jsonlogin)
-            response.raise_for_status()
-            token = response.json().get('token')
-            if not token:
-                # print("Token not found in response")
-                return
-        except requests.exceptions.RequestException as e:
-            # print(f"Error during login request: {e}")
-            return
-
-        headers = {
-            'Authorization': 'Bearer ' + str(token),
-            'Content-Type': 'application/json',
-        }
-
-        started_at_str = zxc[1]
-        started_at = datetime.strptime(started_at_str, "%Y-%m-%dT%H:%M:%S.%f")
-        send_notifications_at = started_at - timedelta(hours=1)
-        send_notifications_at_str = send_notifications_at.strftime("%Y-%m-%dT%H:%M:%S")
-
-        participants_count = int(zxc[2])
-
-        cre = {
-            "name": zxc[0],
-            "ciscoSettings": {
-                "isMicrophoneOn": True,
-                "isVideoOn": True,
-                "isWaitingRoomEnabled": True
-            },
-            "participantsCount": participants_count,
-            "startedAt": zxc[1],
-            "duration": zxc[3],
-            "participants": [],
-            "sendNotificationsAt": send_notifications_at_str,
-            "state": "booked",
-            "organizedBy":{
-                'id': requests.post(url_login, headers=headers_login, json=jsonlogin).json()['user']['id']
+            jsonlogin = {
+                "login": str(users.loc[users['id'].values.tolist().index(message.from_user.id), 'login']),
+                "password": str(users.loc[users['id'].values.tolist().index(message.from_user.id), 'password']),
+                "fingerprint": {}
             }
-        }
 
-        #print("Request data:", json.dumps(cre, indent=4))
+            try:
+                response = requests.post(url_login, headers=headers_login, json=jsonlogin)
+                response.raise_for_status()
+                token = response.json().get('token')
+                if not token:
+                    # print("Token not found in response")
+                    return
+            except requests.exceptions.RequestException as e:
+                # print(f"Error during login request: {e}")
+                return
 
-        try:
-            v = requests.post(url_vcs, headers=headers, json=cre)
-            v.raise_for_status()
+            headers = {
+                'Authorization': 'Bearer ' + str(token),
+                'Content-Type': 'application/json',
+            }
 
-            await message.answer(f'ВКС успешно создана \nСсылка для подключения {v.json()['permalink']}')
+            started_at_str = zxc[1]
+            started_at = datetime.strptime(started_at_str, "%Y-%m-%dT%H:%M:%S.%f")
+            send_notifications_at = started_at - timedelta(hours=1)
+            send_notifications_at_str = send_notifications_at.strftime("%Y-%m-%dT%H:%M:%S")
 
-        except requests.exceptions.RequestException as e:
-            await message.answer("Произошла ошибка при создании ВКС, попробуйте заново.")
+            participants_count = int(zxc[2])
+
+            cre = {
+                "name": zxc[0],
+                "ciscoSettings": {
+                    "isMicrophoneOn": True,
+                    "isVideoOn": True,
+                    "isWaitingRoomEnabled": True
+                },
+                "participantsCount": participants_count,
+                "startedAt": zxc[1],
+                "duration": zxc[3],
+                "participants": [],
+                "sendNotificationsAt": send_notifications_at_str,
+                "state": "booked",
+                "organizedBy":{
+                    'id': requests.post(url_login, headers=headers_login, json=jsonlogin).json()['user']['id']
+                }
+            }
+
+            #print("Request data:", json.dumps(cre, indent=4))
+
+            try:
+                v = requests.post(url_vcs, headers=headers, json=cre)
+                v.raise_for_status()
+
+                await message.answer(f'ВКС успешно создана \nСсылка для подключения {v.json()['permalink']}')
+
+            except requests.exceptions.RequestException as e:
+                # await message.answer("Произошла ошибка при создании ВКС, попробуйте заново.")
+                pass
+    else:
+        await message.answer('Пожалуйста авторизуйтесь /login логин пароль')
 
 
 
